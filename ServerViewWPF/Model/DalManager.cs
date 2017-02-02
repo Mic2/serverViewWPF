79cf7559f2a1691fe78f78a51e9578afd1f3e171
Delta135 <135.delta@gmail.com>
2017-02-02 19:22:11 +0100

    Added old dbman

---
+++ ServerViewWPF/Model/DalManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace App1.ViewModel
+{
+    class DalManager
+    {
+        //singleton
+        private static DalManager _instance = null;
+
+        private DalManager()
+        {
+
+        }
+
+        public static DalManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DalManager();
+                }
+                return _instance;
+            }
+        }
+
+        //NOTE: change '(localdb)\Test_env' to the server and db on remote
+        private string dbCon = @"Data Source=(localdb)\Test_env;Initial Catalog=Test_db;Integrated Security=True";
+        private SqlConnection sqlCon = null;
+
+        //retive data and send it to the requester
+        public string GetName()
+        {
+            throw new NotImplementedException();
+            return null;
+        }
+
+        public IPAddress GetIP()
+        {
+            throw new NotImplementedException();
+            return null;
+        }
+
+        public string GetMAC()
+        {
+            throw new NotImplementedException();
+            return null;
+        }
+
+        public string GetStatus()
+        {
+            throw new NotImplementedException();
+            return null;
+        }
+    }
+}
